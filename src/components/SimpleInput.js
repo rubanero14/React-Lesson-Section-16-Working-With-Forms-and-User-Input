55c51f00909c56fb8689981c24e41a367008c6b0
Rajaruban Rajindram <85555894+rubanero14@users.noreply.github.com>
2023-06-18 01:38:43 +0800

    Managing The Overall Form Validity

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -7,7 +7,31 @@ const SimpleInput = (props) => {
   // Accessing data using useState in form
   // variable to store entered name and state update
   const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
   const [err, setErr] = useState(false);
+
+  // Invalid form gatekeeping logic using React method
+  // const [formIsValid, setFormIsValid] = useState(false);
+
+  // useEffect(() => {
+  //   if (name.trim() === "" && err) {
+  //     return setFormIsValid(false);
+  //   }
+
+  //   return setFormIsValid(true);
+  // }, [err]);
+
+  // Invalid form gatekeeping using JS method
+  let formIsValid = false;
+
+  if (
+    name.trim() !== "" &&
+    email.includes("@") &&
+    email.includes(".") &&
+    !err
+  ) {
+    formIsValid = true;
+  }
 
   // name input watcher function
   const getName = (event) => {
@@ -20,9 +44,23 @@ const SimpleInput = (props) => {
     setErr(false);
   };
 
+  // email input watcher function
+  const getEmail = (event) => {
+    if (
+      event.target.value.trim() === "" ||
+      (!event.target.value.includes("@") && !event.target.value.includes("."))
+    ) {
+      setErr(true);
+    }
+
+    setEmail(event.target.value);
+
+    setErr(false);
+  };
+
   // Input blur handler
   const onInputBlur = (event) => {
-    if (name.trim() === "") {
+    if (event.target.value.trim() === "") {
       return setErr(true);
     }
 
@@ -33,21 +71,38 @@ const SimpleInput = (props) => {
   const formSubmit = (event) => {
     event.preventDefault();
 
-    if (name.trim() === "") {
+    if (
+      name.trim() !== "" &&
+      email.includes("@") &&
+      email.includes(".") &&
+      !err
+    ) {
       return setErr(true);
     }
 
     const nameValue = nameRef.current.value;
+
     console.log("Get name using useRef method: ", nameValue);
     console.log("Get name using useState method: ", name);
+    console.log("Get email using useState method: ", email);
 
     // Resetting name value
     nameRef.current.value = "";
     setErr(false);
+    setEmail("");
     return setName("");
   };
 
-  const nameInputClass = err ? "form-control invalid" : "form-control";
+  const nameInputIsValid = err && name.trim() === "";
+  const emailInputIsValid =
+    err && email.trim() === "" && !email.includes("@") && !email.includes(".");
+
+  const nameInputClass = nameInputIsValid
+    ? "form-control invalid"
+    : "form-control";
+  const emailInputClass = emailInputIsValid
+    ? "form-control invalid"
+    : "form-control";
 
   return (
     <form onSubmit={formSubmit}>
@@ -62,10 +117,27 @@ const SimpleInput = (props) => {
           value={name}
         />
       </div>
-      <div className="form-actions">
-        <button>Submit</button>
+      <div className={emailInputClass}>
+        <input
+          type="email"
+          id="email"
+          placeholder="Insert your email here.."
+          onChange={getEmail}
+          onBlur={onInputBlur}
+          value={email}
+        />
       </div>
-      {err && <p className="error-text">Please fill up the name...</p>}
+      <div className="form-actions">
+        <button type="submit" disabled={!formIsValid}>
+          Submit
+        </button>
+      </div>
+      {err && name.trim() === "" && (
+        <p className="error-text">Please fill up the name...</p>
+      )}
+      {err && email.trim() === "" && (!email.includes("@") || !email.includes(".")) && (
+        <p className="error-text">Please fill up proper the email...</p>
+      )}
     </form>
   );
 };
